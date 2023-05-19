48715f0356ff714c34a5d6d86070d93792844cd0
Yablonskaya Sofya <71564737+b8enly@users.noreply.github.com>
2023-05-19 11:28:13 +0300

    feat: сделана адаптация для мобильной версии основных страниц, fix: поправлены отступы (#24)
    * feat: задана базовая архитектура
    
    * feat: добавлены линтер и преттиер
    
    * feat: добавлен виджет первого блока на главной страницы
    
    * files added
    
    * feat: добавлен второй блок
    
    * fix: вынесен список в отдельный компонент
    
    * fix: внесены правки
    
    Согласно комменатариям после код-ревью
    
    * feat: добавлен блок с проектами на главной странице
    
    * fix: исправлены пути
    
    * fix added
    
    * fix: изменены отступы согласно код-ревью
    
    * feat: добавлена страница проекта, исправлены отступы
    
    * fix: убраны лишние переменные
    
    * fix: внесены правки + начат футер
    
    * fix: добавлены иконки в разделе стек технологий
    
    * fix: стили отредактированы
    
    * feat: добавлена страница ошибки, fix: переверстаны страницы согласно макету, изменены градиенты
    
    * fix: добавлена частично адаптация до главной страницы
    
    * feat: адаптация в процессе
    
    * feat: сделана адаптация футера под мобильные устройства
    
    * fix: исправлены отступы, убран ненужный application на странице
    
    * fix: сделана адаптация для страницы "о нас"
    
    * feat: адаптация страницы Error
    
    ---------
    
    Co-authored-by: Renat <parabell241@gmail.com>

--- src/pages/Project/widgets/ProjectBlock/index.tsx
+++ src/pages/Project/widgets/ProjectBlock/index.tsx
@@ -8,35 +8,35 @@ const ProjectBlock = () => {
   const colors = [''];
 
   return (
-    <div className={'container'}>
-      <div className={classes['project-container']}>
+    <div className={classes['project-container']}>
+      <div className={classes['btn-back']}>
         <SecondButton text={'Назад к проектам'} />
-        <div className={classes['project-info']}>
-          <div className={classes['right-block']}>
-            <p className={classes['project-title']}>About Web</p>
-            <p className={classes['project-years']}>2020 — 2021</p>
-            <p className={classes['project-description']}>
-              Это веб-студия, специализирующаяся на создании пользовательских
-              интерфейсов, разработке веб-сайтов и веб-приложений для клиентов
-              различных отраслей. Команда About Web состоит из опытных
-              веб-разработчиков, дизайнеров и маркетологов, которые обладают
-              большим опытом в создании веб-проектов и работе с последними
-              технологиями.
-            </p>
-            <div className={classes['project-stack']}>
-              <p className={classes['stack-title']}> Стек технологий: </p>
-              {stack.map((item, index) => (
-                <p className={'stack-items'} key={index}>
-                  {item}
-                </p>
-              ))}
-            </div>
-            <div className={classes['btn-container']}>
-              <Button text={'Ссылка на проект'} />
-            </div>
+      </div>
+      <div className={classes['project-info']}>
+        <div className={classes['right-block']}>
+          <p className={classes['project-title']}>About Web</p>
+          <p className={classes['project-years']}>2020 — 2021</p>
+          <p className={classes['project-description']}>
+            Это веб-студия, специализирующаяся на создании пользовательских
+            интерфейсов, разработке веб-сайтов и веб-приложений для клиентов
+            различных отраслей. Команда About Web состоит из опытных
+            веб-разработчиков, дизайнеров и маркетологов, которые обладают
+            большим опытом в создании веб-проектов и работе с последними
+            технологиями.
+          </p>
+          <div className={classes['project-stack']}>
+            <p className={classes['stack-title']}> Стек технологий: </p>
+            {stack.map((item, index) => (
+              <p className={'stack-items'} key={index}>
+                {item}
+              </p>
+            ))}
           </div>
-          <div className={classes['left-block']}></div>
+          <div className={classes['btn-container']}>
+            <Button text={'Ссылка на проект'} />
+          </div>
         </div>
+        <div className={classes['left-block']}></div>
       </div>
     </div>
   );
