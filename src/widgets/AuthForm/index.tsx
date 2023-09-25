b8d85d601fe1beb7cffa19717ce00a96e1b5d6d6
Isas2g <parabell241@gmail.com>
2023-09-25 22:38:29 +0300

    feat: Реализовал авторизацию с сервером (#27)
    * feat: задана базовая архитектура
    
    * feat: добавлены линтер и преттиер
    
    * feat: добавлен виджет первого блока на главной страницы
    
    * fix: исправлены шрифты
    
    * fix: удалены лишние файлы, исправлены конфликты
    
    * fix: файлы стили переделаны на модульные, чтобы не было пересечения классов
    
    * feat: Добавлен container
    
    * fix: Исправлен Header (исправлено выравнивание и добавлен cursor: pointer) и Button (добавлен cursor: pointer)
    
    * feat: Созданы первые два блока страницы О нас
    
    * fix: Страница About переделана на scss модули
    
    * feat: Полностью сделана страница About
    
    * feat: Сделан первый блок страницы Projects без компонентов проектов
    
    * feat: добавлены новые страницы: авторизация, админка проектов, создание проекта, участники проекта
    
    * fix: удален комментарий
    
    * fix: добавлены карточки проектов на страницу проектов
    
    * fix: поправлен футер, добавлены ссылки в хедер
    
    * feat: Доделаны страницы About и Projects (добавлены компоненты)
    
    * fix: исправлен блок с проектами на главной
    
    * fix: Исправлен ProjectsList
    
    * fix: Поправлена верстка страницы About и добавлен Props style для компонента Application
    
    * fix: Поправлена страница Projects
    
    * fix: Поправлена верстка страницы Main, на некоторые компоненты (Paragraph, Subtitle) добавлен Props style
    
    * fix: Поправлена верстка страницы AdminProjects
    
    * feat: Работает функционал компонента AboutBlock
    
    * fix: Добавлен линк на кнопку в компоненте MainProjects
    
    * fix: Убрано ограничение по ширине на App и добавлено ограничение по ширине на container
    
    * fix: Исправлена функция кнопки в AboutBlock
    
    * fix: Убран console log
    
    * fix:
    
    * feat: Сделан бургер и поправлена адаптация на страницах
    
    * fixed styles
    
    * fix: удалил комментарий
    
    * feat: Доделана адаптация всех страниц
    
    * fixed axios settings
    
    ---------
    
    Co-authored-by: Denis Kamchatov <71318867+DenisKamchatov@users.noreply.github.com>
    Co-authored-by: Ренат <renat@MacBook-Air-Renat.local>

--- src/widgets/AuthForm/index.tsx
+++ src/widgets/AuthForm/index.tsx
@@ -1,8 +1,22 @@
 import Input from 'shared/ui/Input';
 import classes from './style.module.scss';
 import Button from 'shared/ui/Button';
+import { Credentials } from 'entities/User/store';
+import { useState } from 'react';
 
-const AuthForm = () => {
+interface Props {
+  handler: ({login, password}: Credentials) => void;
+}
+
+const AuthForm = ({
+  handler
+}: Props) => {
+  const [credentials, setCredentials] = useState<Credentials>({
+    login: '',
+    password: ''
+  });
+
+
   return (
     <div className={`${classes['container']} container`}>
       <h2 className={classes['auth-title']}>Авторизация</h2>
@@ -14,6 +28,7 @@ const AuthForm = () => {
           placeholder="Логин"
           containerStyle={{ marginBottom: 19 }}
           name="login"
+          onChange={(e) => setCredentials({...credentials, login: e.target.value})}
         />
         <Input
           id="password"
@@ -22,9 +37,15 @@ const AuthForm = () => {
           placeholder="Пароль"
           containerStyle={{ marginBottom: 19 }}
           name="password"
+          onChange={(e) => setCredentials({...credentials, password: e.target.value})}
         />
         <span className={classes['forgot-pass-btn']}>забыли пароль?</span>
-        <Button style={{ padding: 10 }} text="Войти" isAction />
+        <Button
+          style={{ padding: 10 }} 
+          text="Войти" 
+          isAction 
+          onClick={() => {handler(credentials)}}
+        />
       </div>
     </div>
   );
