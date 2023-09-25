26d2d8594a7416771905cc0e559c6a8a713f9c2f
Ренат <renat@MacBook-Air-Renat.local>
2023-09-25 20:59:22 +0300

    fixed axios settings

--- src/shared/api/index.tsx
+++ src/shared/api/index.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://45.141.78.221:3000/api/v1/',
+  baseURL: 'https://45.141.78.221/api/v1/',
   timeout: 5000,
   headers: { 'Content-Type': 'application/json' },
 });
