0fed2954bd611efa181e949d33763e164f858148
Yablonskaya Sofya <sofa512@yandex.ru>
2023-10-18 17:04:19 +0300

    fix: переезжаем назад на https

--- src/shared/api/index.tsx
+++ src/shared/api/index.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 // import https from 'https';
 
 const instance = axios.create({
-  baseURL: 'http://viotrina.ru/api/v1/',
+  baseURL: 'https://viotrina.ru/api/v1/',
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
@@ -12,9 +12,9 @@ const instance = axios.create({
   // }),
   withCredentials: true,
   proxy: {
-    host: 'http://45.141.78.221/api/v1/',
+    host: 'https://45.141.78.221/api/v1/',
     port: 443,
-    protocol: 'http',
+    protocol: 'https',
   },
 });
 
